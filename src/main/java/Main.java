4d591bee4e0472fa8b8848c5a10495281f9f4562
2dino2 <michal.globtrotter@gmail.com>
2023-06-21 01:44:47 +0200

    Dodałem randomowe nazwy używając Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.kohsuke.randname.RandomNameGenerator;
 
 public class Main {
     public static void main(String[] args) {
         ChuckNorrisQuotes jokes = new ChuckNorrisQuotes();
         System.out.println(jokes.getRandomQuote());
+        RandomNameGenerator randomNameGenerator = new RandomNameGenerator();
+        System.out.println(randomNameGenerator.next());
     }
 }
