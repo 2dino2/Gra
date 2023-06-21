1cb5a80cd4bd0e59fe99ea7b92e51634c2916585
2dino2 <michal.globtrotter@gmail.com>
2023-06-21 03:03:54 +0200

    Zrobiłem Test i zmieniłem troche wojnę

---
+++ src/test/java/Test/TestWojny.java
@@ -0,0 +1,15 @@
+package Test;
+
+import Przeciwnicy.Wschod;
+import Przeciwnicy.Zachod;
+import Wojna.Atak;
+
+public class TestWojny {
+    public static void main(String[] args) {
+        Wschod wschod = new Wschod(100,20);
+        Zachod zachod = new Zachod(80, 30);
+        Atak.atak(wschod, zachod);
+        Atak.atak(zachod, wschod);
+        Atak.wojna(wschod, zachod);
+    }
+}
