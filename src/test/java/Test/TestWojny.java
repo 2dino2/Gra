022e8f2771b44012fe7a028aff0f601311a5c763
2dino2 <michal.globtrotter@gmail.com>
2023-06-21 03:06:00 +0200

    Zmieniłem trochę wartości ataku

--- src/test/java/Test/TestWojny.java
+++ src/test/java/Test/TestWojny.java
@@ -6,7 +6,7 @@ import Wojna.Atak;
 
 public class TestWojny {
     public static void main(String[] args) {
-        Wschod wschod = new Wschod(100,20);
+        Wschod wschod = new Wschod(100,30);
         Zachod zachod = new Zachod(80, 30);
         Atak.atak(wschod, zachod);
         Atak.atak(zachod, wschod);
