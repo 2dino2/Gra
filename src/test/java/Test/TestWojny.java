09aaa6157ae660faf0e7c9bb7c1eca6562d7bbd5
2dino2 <michal.globtrotter@gmail.com>
2023-06-21 11:19:58 +0200

    Dodałem komentarze w TestWojny

--- src/test/java/Test/TestWojny.java
+++ src/test/java/Test/TestWojny.java
@@ -6,8 +6,11 @@ import Wojna.Atak;
 
 public class TestWojny {
     public static void main(String[] args) {
+        //tworze przeciwnika wschód ze zdrowiem 100 i atakiem 30
         Wschod wschod = new Wschod(100,30);
+        //tworzę przeciwnika zachod ze zdrowiem 80 i atakiem 30
         Zachod zachod = new Zachod(80, 30);
+        //używam ataków i potem wojny, żeby jeden przeciwnik zginął
         Atak.atak(wschod, zachod);
         Atak.atak(zachod, wschod);
         Atak.wojna(wschod, zachod);
